a841905cb9d2fdda722374587df7e8419737f0f4
Andrey Vetrov <vesstrov4942@gmail.com>
2017-09-12 17:55:59 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+ function openbox(){
+ 	display = document.getElementById('form').style.display;
+ 	if(display=='none'){
+ 		document.getElementById('form').style.display='block';
+ 	} else {
+ 		document.getElementById('form').style.display='none';
+ 	}
+ }
